a32baf790d6ea144d5d3eed178083fd749051396
hazem_m.o <hm3185@fayoum.edu.eg>
2024-05-09 14:49:29 +0300

    add gaps to logos

--- src/hazem-components/LogosComp-components/Card.jsx
+++ src/hazem-components/LogosComp-components/Card.jsx
@@ -5,14 +5,14 @@ function Card({ Data}) {
         <div className="all container mx-auto text-white bg-black rounded-2xl mt-7">
             <div className="header text-center py-8">
                 <div className="container mx-auto">
-                    <h1 className="  text-xl">
+                    <h1 className="  text-xl leading-relaxed">
                         { Data.Header.h1 ? Data.Header.h1:"Null"} <span className=" text-black bg-white px-2 py-1 rounded-3xl italic font-bold">{Data.Header.span ? Data.Header.span:"Null" }</span></h1>
                 </div>
             </div>
             <div className="content relative w-full p-2 overflow-hidden">
                 <div className="overlay absolute z-10  top-0 left-0 w-full h-full bg-[linear-gradient(to_right,_black,_transparent_30%,_transparent_50%,_transparent,_black)]   bottom-[0%]  right-[0%]"></div>
                 <div className="container relative" >
-                    <div className="top flex justify-evenly overflow-hidden ">
+                    <div className="top flex items-center gap-3 overflow-hidden ">
                         <div className="icon">
                             { Data.Content.top.icon1 ? Data.Content.top.icon1:"Null"}
                         </div>
@@ -32,7 +32,7 @@ function Card({ Data}) {
                         { Data.Content.top.icon6 ? Data.Content.top.icon6:"Null"}
                         </div>
                     </div>
-                    <div className="bottom flex justify-evenly mt-2 overflow-hidden ">
+                    <div className="bottom flex items-center gap-3 mt-2 overflow-hidden ">
                         <div className="icon">
                         { Data.Content.bottom.icon1 ? Data.Content.bottom.icon1:"Null"}
                         </div>
