f7cb0a51be9e7975a2a6fe459b586ae7a1b62e61
kholoudxs55kh <kfattem@gmail.com>
2024-05-18 03:34:50 +0300

    fix(frontend): Enhance Code using useForm() hook and it's validations

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import { FaPhone, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"; // Importing icons from react-icons
+import { FaPhone, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 
 export default function Contact1_Inner() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const CompanyContactData = [
     {
       id: 1,
@@ -23,74 +25,34 @@ export default function Contact1_Inner() {
     },
   ];
 
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
-  });
-
-  const [errors, setErrors] = useState({});
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const validate = () => {
-    let tempErrors = {};
-    if (!formData.firstName.match(/^[a-z]+$/i))
-      tempErrors.firstName = "First Name should be a valid name";
-    if (!formData.lastName.match(/^[a-z]+$/i))
-      tempErrors.lastName = "Last Name should be a valid name";
-    if (!formData.email.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i))
-      tempErrors.email = "Email is not valid";
-    if (!formData.phone.match(/^[0-9]+$/))
-      tempErrors.phone = "Phone Number should be numeric";
-    if (!formData.message) tempErrors.message = "Message cannot be empty";
-
-    setErrors(tempErrors);
-    return Object.keys(tempErrors).length === 0;
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    if (validate()) {
-      console.log(formData);
-    }
+  const onSubmit = async (data) => {
+    console.log("data",data);
   };
 
   return (
-    <div className="bg-gray-100 py-12 flex justify-center ">
+    <div className="bg-gray-100 py-12 flex justify-center">
       <div className="flex flex-col md:flex-row gap-12 w-8/12">
         <div className="container mx-auto bg-white p-12 rounded-3xl md:w-1/2">
           <div className="flex flex-col gap-2">
             <h6 className="text-TextColor font-bold">Get in Touch</h6>
-            <h2>let's Chat, Contact with Us</h2>
+            <h2>Let's Chat, Contact with Us</h2>
             <p className="text-gray-500">
-              Have any questions or feedback? We're here to help. Send us a
-              message, We'll get back to you within 24 hours.
+              Have any questions or feedback? We're here to help. Send us a message, and we'll get back to you within 24 hours.
             </p>
           </div>
-          <form className="flex flex-col gap-4 mt-8" onSubmit={onSubmit}>
+          <form className="flex flex-col gap-4 mt-8" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex justify-between">
               <div className="flex flex-col gap-2 w-5/12">
-                <label htmlFor="firstName" className="">First Name</label>
+                <label htmlFor="firstName">First Name</label>
                 <input
                   type="text"
                   id="firstName"
-                  name="firstName"
                   placeholder="First Name"
-                  value={formData.firstName}
-                  onChange={handleChange}
+                  {...register("firstName", { required: "First Name is required", pattern: { value: /^[a-z]+$/i, message: "First Name should be a valid name" } })}
                   className="p-2 border border-gray-300 rounded-md"
                 />
                 {errors.firstName && (
-                  <span className="text-red-500">{errors.firstName}</span>
+                  <span className="text-red-500">{errors.firstName.message}</span>
                 )}
               </div>
               <div className="flex flex-col gap-2 w-1/2">
@@ -98,14 +60,12 @@ export default function Contact1_Inner() {
                 <input
                   type="text"
                   id="lastName"
-                  name="lastName"
-                    placeholder="Last Name"
-                  value={formData.lastName}
-                  onChange={handleChange}
+                  placeholder="Last Name"
+                  {...register("lastName", { required: "Last Name is required", pattern: { value: /^[a-z]+$/i, message: "Last Name should be a valid name" } })}
                   className="p-2 border border-gray-300 rounded-md"
                 />
                 {errors.lastName && (
-                  <span className="text-red-500">{errors.lastName}</span>
+                  <span className="text-red-500">{errors.lastName.message}</span>
                 )}
               </div>
             </div>
@@ -114,14 +74,12 @@ export default function Contact1_Inner() {
               <input
                 type="email"
                 id="email"
-                name="email"
                 placeholder="yourname@company.com"
-                value={formData.email}
-                onChange={handleChange}
+                {...register("email", { required: "Email is required", pattern: { value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i, message: "Email is not valid" } })}
                 className="p-2 border border-gray-300 rounded-md"
               />
               {errors.email && (
-                <span className="text-red-500">{errors.email}</span>
+                <span className="text-red-500">{errors.email.message}</span>
               )}
             </div>
             <div className="flex flex-col gap-2">
@@ -129,28 +87,24 @@ export default function Contact1_Inner() {
               <input
                 type="text"
                 id="phone"
-                name="phone"
                 placeholder="+1 (123) 456-7890"
-                value={formData.phone}
-                onChange={handleChange}
+                {...register("phone", { required: "Phone Number is required", pattern: { value: /^[0-9]+$/, message: "Phone Number should be numeric" } })}
                 className="p-2 border border-gray-300 rounded-md"
               />
               {errors.phone && (
-                <span className="text-red-500">{errors.phone}</span>
+                <span className="text-red-500">{errors.phone.message}</span>
               )}
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="message">Message</label>
               <textarea
                 id="message"
-                name="message"
-                value={formData.message}
                 placeholder="Type your message..."
-                onChange={handleChange}
+                {...register("message", { required: "Message cannot be empty" })}
                 className="p-2 border border-gray-300 rounded-md"
               ></textarea>
               {errors.message && (
-                <span className="text-red-500">{errors.message}</span>
+                <span className="text-red-500">{errors.message.message}</span>
               )}
             </div>
             <button
@@ -164,7 +118,7 @@ export default function Contact1_Inner() {
         <div className="flex flex-col justify-center items-center mx-auto md:w-1/2 mt-8 md:mt-0">
           <div className="flex flex-col gap-4 items-center w-full rounded-2xl">
             <img
-            className="w-full rounded-2xl"
+              className="w-full rounded-2xl"
               src="https://media.istockphoto.com/id/841093480/photo/contact-us.jpg?s=612x612&w=0&k=20&c=SUs3dkEh_NgwejMA9i03wMTPz4UcEO2JkySTiPkT6u8="
               alt="Contact Us Image"
             />
@@ -188,7 +142,7 @@ function ContactItem({ item }) {
       <div className="flex items-center justify-center bg-contactbg p-4 m-3 md:m-2 md:p-2 rounded-full">
         {item.icon}
       </div>
-      <div className="flex flex-col p-2 ">
+      <div className="flex flex-col p-2">
         <span className="text-white font-bold">{item.title}</span>
         <span className="text-gray-300 md:text-gray md:text-xs">{item.value}</span>
       </div>
