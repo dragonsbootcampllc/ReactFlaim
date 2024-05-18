9423677d3e56d2f1768614f59e5add46b113b0b1
DarkCode662432 <ff9371668@gmail.com>
2024-05-19 00:21:05 +0300

    [enhance]

--- src/Pages/notFound/notFound.jsx
+++ src/Pages/notFound/notFound.jsx
@@ -2,7 +2,7 @@ import img from "./2024-05-18_031346.png";
 
 const NotFound_page = () => {
   return (
-    <div className="flex flex-col items-center text-center p-5 md:flex-row md:justify-around md:text-left lg:p-10 max-w-screen-2xl mx-auto bg-slate-100">
+    <div className="flex flex-col h-screen w-screen items-center text-center p-5 md:flex-row md:justify-around md:text-left lg:p-10 max-w-screen-2xl mx-auto bg-slate-100">
       <div className="max-w-lg">
         <h1 className="text-5xl font-bold mb-4 text-blue-600">404 ERROR</h1>
         <h1 className="text-2xl font-bold mb-2">
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ComponentsPlayGround from './Pages/ComponentsPlayGround';
-import SecondPage from './Pages/SecondPage';
-import PageN5 from './Pages/PageN5';
-import PageN7 from './Pages/PageN7';
-import Pagen8 from './Pages/PageN8';
-
 
 
 // Style Sheets Imports
 import '/public/assets/css/index.css';
+import NotFound_page from './Pages/notFound/NotFound';
 
 // Application Component
 export function Application() {
@@ -19,10 +15,7 @@ export function Application() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ComponentsPlayGround />} />
-        <Route path="/secondPage" element={<SecondPage />} />
-        <Route path="/pageN5" element={<PageN5 />} />
-        <Route path="/pageN7" element={<PageN7 />} />
-        <Route path="/pageN8" element={<Pagen8 />} />
+        <Route path="*" element={<NotFound_page />} />
       </Routes>
     </BrowserRouter>
   );
