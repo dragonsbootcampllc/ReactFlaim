946df55582678623280bb3cc81bb39981c07e730
hazem_m.o <hm3185@fayoum.edu.eg>
2024-05-22 22:40:14 +0300

    New component added "Hero Component"

---
+++ src/Pages/HeroComponent.jsx
@@ -0,0 +1,8 @@
+import HeroComponent_Wrapper from "../underDevelop/HeroComponent/HeroComponent_Parent";
+
+function HeroComponent() {
+    return (
+        <HeroComponent_Wrapper />
+    )
+}
+export default HeroComponent;
